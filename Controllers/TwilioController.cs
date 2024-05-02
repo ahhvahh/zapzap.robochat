9b6e9f6acb2098c43e036cb4886dcadc4059faa2
WildBillBiccup <84429613+ahhvahh@users.noreply.github.com>
2024-05-01 22:58:59 -0300

    primeira versao

---
+++ Controllers/TwilioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio.TwiML;  // Certifique-se de ter o pacote Twilio instalado
+
+namespace zappzap.robochat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TwilioController : ControllerBase
+    {
+        [HttpPost("ReceiveMessage")]
+        public ContentResult ReceiveMessage([FromForm] string From, [FromForm] string Body)
+        {
+            var response = new MessagingResponse();
+            if (Body.ToLower().Contains("chatrobot"))
+            {
+                var message = "Qual Robô deseja falar?\n1. Nenhum\n2. Parque Jurubatuba\n3. gpt";
+                response.Message(message);
+            }
+            else if (Body.ToLower() == "parque jurubatuba")
+            {
+                response.Message("Este serviço não está pronto!");
+            }
+
+            return Content(response.ToString(), "text/xml");
+        }
+    }
+}
